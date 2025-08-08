351f394ec8c0a370712780396da5b5ee403e11b0
Sigsign <sig@signote.cc>
2025-08-09 00:13:52 +0900

    Implement redirector

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,26 @@
 export default defineContentScript({
-  matches: ["*://*.google.com/*"],
-  main() {
-    console.log("Hello content.");
+  matches: ["https://www.youtube.com/*"],
+  allFrames: false,
+  runAt: "document_start",
+  main(ctx) {
+    const shortsID = getShortsID(window.location.pathname);
+    if (shortsID !== "") {
+      window.location.replace(`https://www.youtube.com/watch?v=${shortsID}`);
+    }
+
+    ctx.addEventListener(document, "yt-navigate-start", () => {
+      const shortsID = getShortsID(window.location.pathname);
+      if (shortsID !== "") {
+        window.location.replace(`https://www.youtube.com/watch?v=${shortsID}`);
+      }
+    });
   },
 });
+
+function getShortsID(pathname: string): string {
+  const segments = pathname.split("/");
+  if (segments.length === 3 && segments[1] === "shorts") {
+    return segments[2];
+  }
+  return "";
+}
